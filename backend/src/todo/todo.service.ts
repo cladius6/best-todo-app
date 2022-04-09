5c7bd9bdb660c31df90621277f398e9fe7e49145
Klaudiusz <d1f1.6@protonmail.com>
2022-04-09 18:19:30 +0200

    Removing and adding todo tests.

--- backend/src/todo/todo.service.ts
+++ backend/src/todo/todo.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  private readonly todo;
+
+  constructor() {
+    this.todo = new TodoListSingleton();
+  }
+
+  getTodo() {
+    return this.todo;
+  }
+}
+
+export class TodoListSingleton {
+  private todos = [];
+
+  add(todo) {
+    todo.id = this.todos.length + 1;
+    this.todos.push(todo);
+  }
+
+  remove(id: number) {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+
+  getAll() {
+    return this.todos;
+  }
+}
