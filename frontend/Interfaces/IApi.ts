956b441b3eb466fc836ab26f85274bbe4bad0077
Krystian Kat <krystian.kat@protonmail.com>
2022-04-09 21:28:53 +0200

    refactor: change order in IApi.ts

--- frontend/Interfaces/IApi.ts
+++ frontend/Interfaces/IApi.ts
@@ -1,9 +1,9 @@
 import { ITasksList } from './ITask';
 
-export interface IGetTasksListResponse {
-  tasksList: ITasksList;
-}
-
 export interface IApi {
   getTasksList(): Promise<IGetTasksListResponse>;
+}
+
+export interface IGetTasksListResponse {
+  tasksList: ITasksList;
 }
