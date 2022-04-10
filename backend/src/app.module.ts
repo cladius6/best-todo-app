d8094ad2ff234c0f792380673010df2e57e15ed9
Klaudiusz <d1f1.6@protonmail.com>
2022-04-10 18:35:00 +0200

    TypeORM config and connection initialized

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
+import { TodoEntity } from './todo/entity/todo.entity';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmConfigService } from './typeorm/typeorm.config.service';
+import * as path from 'path';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: path.resolve(__dirname, '../../.env.development'),
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot()],
+      useClass: TypeOrmConfigService,
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
