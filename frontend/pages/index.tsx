0eacf265e9ddc374d64ee1d3465ebc93b72ec678
Krystian Kat <krystian.kat@protonmail.com>
2022-04-11 10:22:30 +0200

    style: add dumb styling

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -122,200 +122,217 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <h1>BEST TODO APP</h1>
+    <div
+      className="h-screen flex flex-col 
+                    items-center justify-center"
+    >
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+        <h1 className="text-3xl font-bold mb-6 m-auto">BEST TODO APP</h1>
 
-      <form id="formAddTask" onSubmit={submitNewTask}>
-        <div className={styles.control}>
-          <label htmlFor="newTask" className={styles.label}>
-            add task
-          </label>
+        <form id="formAddTask" onSubmit={submitNewTask} className="flex mb-4">
+          <div className="">
+            <label
+              htmlFor="newTask"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            ></label>
+          </div>
 
-          <input
-            id="newTask"
-            type="text"
-            required
-            value={enteredTitleNewTask}
-            onChange={titleChangeNewTaskHandler}
-            className={`${styles.input} form-control`}
-          />
-        </div>
-        <div className="">
-          <button
-            id="buttonSubmitNewTask"
-            type="submit"
-            className={`button button_task button_task--add`}
-          >
-            ADD
-          </button>
-        </div>
-      </form>
+          <div className="flex justify-between w-80">
+            <input
+              id="newTask"
+              type="text"
+              required
+              value={enteredTitleNewTask}
+              onChange={titleChangeNewTaskHandler}
+              className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+            />
 
-      <ul id="tasksList">
-        {tasksList &&
-          tasksList.map((task, index) => {
-            if (
-              tasksTypeToDisplay === TasksType.Completed &&
-              task.status === StatusType.Active
-            )
-              return;
-            if (
-              tasksTypeToDisplay === TasksType.Uncompleted &&
-              task.status === StatusType.Completed
-            )
-              return;
-            return editedTask?.id !== task.id ? (
-              <li
-                className={`task task--${task.id}`}
-                key={`task task--${task.id}`}
-              >
+            <button
+              id="buttonSubmitNewTask"
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              ADD
+            </button>
+          </div>
+        </form>
+
+        <ul id="tasksList">
+          {tasksList &&
+            tasksList.map((task, index) => {
+              if (
+                tasksTypeToDisplay === TasksType.Completed &&
+                task.status === StatusType.Active
+              )
+                return;
+              if (
+                tasksTypeToDisplay === TasksType.Uncompleted &&
+                task.status === StatusType.Completed
+              )
+                return;
+              return editedTask?.id !== task.id ? (
                 <div
-                  className={`task_name task_name--${task.id} ${
-                    task.status === StatusType.Completed
-                      ? 'task_name--completed'
-                      : 'task_name--uncompleted'
-                  }`}
+                  className={`task task--${task.id} mb-3`}
+                  key={`task task--${task.id}`}
                 >
-                  {task.title}
-                </div>
-                <div
-                  className={`task_control-panel task_control-panel--${task.id}`}
-                >
-                  <button
-                    id="buttonTaskToggleDone"
-                    className={`button button_task button_task--done`}
-                    onClick={() => {
-                      setTasksList(
-                        editTaskInTasksList(tasksList, {
+                  <li
+                    className={`task_name task_name--${task.id} mb-3 text-xl ${
+                      task.status === StatusType.Completed
+                        ? 'task_name--completed'
+                        : 'task_name--uncompleted'
+                    }`}
+                  >
+                    {task.title}
+                  </li>
+                  <div
+                    className={`task_control-panel task_control-panel--${task.id} flex justify-between text-xs`}
+                  >
+                    <button
+                      id="buttonTaskToggleDone"
+                      className="text-gray-400 hover:text-green-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      onClick={() => {
+                        setTasksList(
+                          editTaskInTasksList(tasksList, {
+                            ...tasksList[index],
+                            status:
+                              tasksList[index].status === StatusType.Active
+                                ? StatusType.Completed
+                                : StatusType.Active,
+                          })
+                        );
+                        TodoApi.editTodo({
                           ...tasksList[index],
                           status:
                             tasksList[index].status === StatusType.Active
                               ? StatusType.Completed
                               : StatusType.Active,
-                        })
-                      );
-                      TodoApi.editTodo({
-                        ...tasksList[index],
-                        status:
-                          tasksList[index].status === StatusType.Active
-                            ? StatusType.Completed
-                            : StatusType.Active,
-                      });
-                    }}
-                  >
-                    done
-                  </button>
+                        });
+                      }}
+                    >
+                      done
+                    </button>
 
-                  <button
-                    id="buttonTaskEdit"
-                    className={`button button_task button_task--edit`}
-                    onClick={() => {
-                      if (editedTask !== null) return;
-                      setEditedTask(tasksList[index]);
-                    }}
-                  >
-                    edit
-                  </button>
+                    <button
+                      id="buttonTaskEdit"
+                      className=" hover:text-orange-700 text-gray-400 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      onClick={() => {
+                        if (editedTask !== null) return;
+                        setEditedTask(tasksList[index]);
+                      }}
+                    >
+                      edit
+                    </button>
 
-                  <button
-                    id="buttonTaskDelete"
-                    className={`button button_task button_task--delete`}
-                    onClick={() => {
-                      setTasksList(
-                        deleteTaskInTasksList(tasksList, tasksList[index])
-                      );
-                      TodoApi.deleteTodo(tasksList[index].id);
-                    }}
-                  >
-                    delete
-                  </button>
+                    <button
+                      id="buttonTaskDelete"
+                      className="text-gray-400 hover:text-red-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      onClick={() => {
+                        setTasksList(
+                          deleteTaskInTasksList(tasksList, tasksList[index])
+                        );
+                        TodoApi.deleteTodo(tasksList[index].id);
+                      }}
+                    >
+                      delete
+                    </button>
+                  </div>
                 </div>
-              </li>
-            ) : (
-              <form
-                key={`form-change-title--task-${task.id}`}
-                className={`form-change-title form-change-title--task-${task.id}`}
-                onSubmit={() => {
-                  setTasksList(
-                    editTaskInTasksList(tasksList, {
-                      ...editedTask,
-                      title:
+              ) : (
+                <form
+                  key={`form-change-title--task-${task.id}`}
+                  className={`form-change-title form-change-title--task-${task.id}`}
+                  onSubmit={() => {
+                    setTasksList(
+                      editTaskInTasksList(tasksList, {
+                        ...editedTask,
+                        title:
+                          enteredTitleEditedTask !== null
+                            ? enteredTitleEditedTask
+                            : editedTask.title,
+                      })
+                    );
+                    if (enteredTitleEditedTask !== editedTask.title) {
+                      TodoApi.editTodo({
+                        ...editedTask,
+                        title: enteredTitleEditedTask,
+                      });
+                    }
+
+                    setEditedTask(null);
+                    setEnteredTitleEditedTask(null);
+                  }}
+                >
+                  <div className="flex">
+                    <label
+                      htmlFor="editedTask"
+                      className={styles.label}
+                    ></label>
+
+                    <input
+                      id="editedTask"
+                      type="text"
+                      required
+                      value={
                         enteredTitleEditedTask !== null
                           ? enteredTitleEditedTask
-                          : editedTask.title,
-                    })
-                  );
-                  if (enteredTitleEditedTask !== editedTask.title) {
-                    TodoApi.editTodo({
-                      ...editedTask,
-                      title: enteredTitleEditedTask,
-                    });
-                  }
+                          : task.title
+                      }
+                      onChange={titleChangeEditedTaskHandler}
+                      className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                    />
 
-                  setEditedTask(null);
-                  setEnteredTitleEditedTask(null);
-                }}
-              >
-                <div className={styles.control}>
-                  <label htmlFor="editedTask" className={styles.label}>
-                    edit:
-                  </label>
+                    <button
+                      id="buttonEditedTaskSave"
+                      type="submit"
+                      className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    >
+                      save
+                    </button>
+                  </div>
+                </form>
+              );
+            })}
+        </ul>
+        <div className="flex justify-between">
+          <button
+            id="buttonTypeDisplayAll"
+            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => {
+              setTasksTypeToDisplay(TasksType.All);
+            }}
+          >
+            ALL
+          </button>
 
-                  <input
-                    id="editedTask"
-                    type="text"
-                    required
-                    value={
-                      enteredTitleEditedTask !== null
-                        ? enteredTitleEditedTask
-                        : task.title
-                    }
-                    onChange={titleChangeEditedTaskHandler}
-                    className={`form-change-title_control`}
-                  />
-                </div>
+          <button
+            id="buttonTypeDisplayTodo"
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => {
+              setTasksTypeToDisplay(TasksType.Uncompleted);
+            }}
+          >
+            TODO
+          </button>
 
-                <button
-                  id="buttonEditedTaskSave"
-                  type="submit"
-                  className={`button button_task button_task--edit`}
-                >
-                  save
-                </button>
-              </form>
-            );
-          })}
-      </ul>
-
-      <button
-        id="buttonTypeDisplayAll"
-        className={`button button_type-display button_type-display--all`}
-        onClick={() => {
-          setTasksTypeToDisplay(TasksType.All);
-        }}
-      >
-        ALL
-      </button>
-
-      <button
-        id="buttonTypeDisplayTodo"
-        className={`button button_type-display button_type-display--todo`}
-        onClick={() => {
-          setTasksTypeToDisplay(TasksType.Uncompleted);
-        }}
-      >
-        TODO
-      </button>
-
-      <button
-        id="buttonTypeDisplayDone"
-        className={`button button_type-display button_type-display--done`}
-        onClick={() => {
-          setTasksTypeToDisplay(TasksType.Completed);
-        }}
-      >
-        DONE
-      </button>
+          <button
+            id="buttonTypeDisplayDone"
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => {
+              setTasksTypeToDisplay(TasksType.Completed);
+            }}
+          >
+            DONE
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
